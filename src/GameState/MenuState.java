aca6652be3932b80be25419acb3f03f1afeacfc1
Henzhwang <36927967+Henzhwang@users.noreply.github.com>
2019-06-05 10:57:57 -0400

    Update MenuState.java

--- src/GameState/MenuState.java
+++ src/GameState/MenuState.java
@@ -5,20 +5,36 @@
  */
 package GameState;
 
+import Graphics.Font;
+import Graphics.Sprite;
 import Utility.KeyHandler;
+import Utility.Vector2d;
+import java.awt.Color;
 import java.awt.Graphics2D;
-import Graphics.Background;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Kyle's PC
  */
 public class MenuState extends GameState {
-
-    private Background menuBackground, startBackground, currentBackground;
     
+    //private Font font;
+    private String pokemon;
+    private String playerName;
+    private int money;
+    private String moneyAmount;
+    private Sprite pokeBall;
+    private Font Sprites;
+    
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        //font = new Font("Font/font.png", 111, 111);
+        pokemon = "Pokemon";
+        pokeBall = new Sprite("Sprites/pokemonBall.png.png");
+        //JOptionPane.showMessageDialog(null, pokeBall);
     }
 
     @Override
@@ -33,7 +49,16 @@ public class MenuState extends GameState {
 
     @Override
     public void render(Graphics2D g) {
-        
+        g.setColor(Color.white);
+        g.fillRect(600, 0, 200, 640);
+        Sprite.drawArray(g, font, "Menu", new Vector2d(625, 75), 48, 48, 36, 0);
+        Sprite.drawArray(g, font, pokemon, new Vector2d(610, 170), 24, 24, 20, 0);
+        Sprite.drawImage(g, pokeBall.getSpriteSheet(), new Vector2d(700, 170), 24, 24);
+        Sprite.drawArray(g, font, "Bag", new Vector2d(610, 230), 24, 24, 20, 0);
+        Sprite.drawArray(g, font, "Save", new Vector2d(610, 290), 24, 24, 20, 0);
+        Sprite.drawArray(g, font, "Exit", new Vector2d(610, 350), 24, 24, 20, 0);
+        //Sprite.drawArray(g, font, playerName, new Vector2d(600, 150), 16, 16, 16, 0);
+        //Sprite.drawArray(g, font, moneyAmount, new Vector2d(600, 150), 16, 16, 16, 0);
     }
     
 }
